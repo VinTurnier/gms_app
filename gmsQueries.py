d6caf95390eff9cfd167284c899d6bea060b5450
Vincent Turnier <vincentturnier@gmail.com>
2019-06-04 22:11:08 -0400

    Plugin Phase Complete, Loading Test...

--- gmsQueries.py
+++ gmsQueries.py
@@ -1,30 +1,31 @@
+from private import Keys
+import mysql.connector
+from collections import namedtuple
 
 
 class Query:
 
-	def __init__(self):
-		pass
 
 	def displayCRX(self,id_citern):
+		self.refresh_db()
 		command = "SELECT * FROM crx WHERE idCitern = \'"+id_citern+"\'"
-		cursor = self.db.cursor()
-		cursor.execute(command)
-		results = cursor.fetchall()
+		self.cursor.execute(command)
+		results = self.cursor.fetchall()
 		if results == []:
 			return 'error' #error code not described yet
-
+			self.db.close()
 		else:
+			self.db.close()
 			return results[0]
 
 
 	def measurements(self, station_id, measurements = None):
 	#1) collects the measurements
 	#2) Adds them to the database
-
+		self.refresh_db()
 		command = "SELECT id FROM fuelDeposit WHERE station_id = \'"+str(station_id)+"\'"
-		cursor = self.db.cursor()
-		cursor.execute(command)
-		id = cursor.fetchall()
+		self.cursor.execute(command)
+		id = self.cursor.fetchall()
 		fuelDeposit_id = id[len(id)-1][0]
 
 		command = "INSERT INTO measurements (station_id, fuelDeposit_id, compt, fuelType, rHeight, dHeight) VALUES (%s,%s,%s,%s,%s,%s)"
@@ -37,43 +38,55 @@ class Query:
 
 		for measurement in measurements:
 			if na_count!=5:
-				values = tuple([station_id, fuelDeposit_id]+measurement)
-				cursor.execute(command,values)
-				self.db.commit()
-				return True
+				if 'N/A' in measurement:
+					continue
+				else:
+					values = tuple([station_id, fuelDeposit_id]+measurement)
+					self.cursor.execute(command,values)
+					self.db.commit()
 			else:
+				self.db.close()
 				return 'er0000'
 
+		self.db.close()
+		return True
+
+
 	def fuelDeposit(self, fuelDeposit_data = None):
+		self.refresh_db()
 		FuelDeposit = namedtuple('FuelDeposit','station_id id_citern date_added')
 		fuelDeposit_data = FuelDeposit._make(tuple(fuelDeposit_data))
 
 		command = "INSERT INTO fuelDeposit(station_id, idCitern, date_added) VALUES (%s,%s,%s)"
 		if '' in fuelDeposit_data:
+			self.db.close()
 			return 'er0000'
 		else:
 			values = (fuelDeposit_data.station_id,
 						fuelDeposit_data.id_citern,
 						fuelDeposit_data.date_added)
-			cursor = self.db.cursor()
-			cursor.execute(command,values)
+			self.cursor.execute(command,values)
 			self.db.commit()
+			self.db.close()
 
 
 	def inventory(self, inventoryData = None):
 	#1) checks the inventory
 	#2) add data to the database
+		self.refresh_db()
 		command = "INSERT INTO inventory (station_id, tank_id, stockBoD, fuelDeposit, sales, stockEoD, date_added) VALUES (%s,%s,%s,%s,%s,%s,%s)"
 		if "N/A" in inventoryData:
+			self.db.close()
 			return 'er0000'
 		else:
 			values = tuple(inventoryData)
-			cursor = self.db.cursor()
-			cursor.execute(command,values)
+			self.cursor.execute(command,values)
 			self.db.commit()
+			self.db.close()
 
-	def displayData(self, table, periods=''):
+	def displayData(self, station_id,table, periods=''):
 	#1) displays the information base on the critaria
+		self.refresh_db()
 		period = {"1 Day":"1 DAY",
 					"5 Days": "5 DAY",
 					"1 Month": "1 MONTH",
@@ -92,7 +105,7 @@ class Query:
 						FROM measurements \
 						JOIN fuelDeposit \
 						ON (measurements.fuelDeposit_id = fuelDeposit.id) \
-						WHERE fuelDeposit.station_id = \''+str(self.stationId)+'\' \
+						WHERE fuelDeposit.station_id = \''+str(station_id)+'\' \
 						AND date_added BETWEEN(CURDATE() - INTERVAL '+period[periods]+') AND CURDATE()',
 				'Inventory': 'SELECT tank.name, \
 							inventory.stockBoD, \
@@ -102,7 +115,7 @@ class Query:
 							inventory.date_added \
 							FROM tank JOIN inventory \
 							ON (tank.id = inventory.tank_id) \
-							WHERE tank.station_id = \''+str(self.stationId)+'\' \
+							WHERE tank.station_id = \''+str(station_id)+'\' \
 							AND date_added BETWEEN(CURDATE() - INTERVAL '+period[periods]+') AND CURDATE()',
 				'Fuel Deposit': 'SELECT fuelDeposit.idCitern, \
 						truckCapacity.crx1+truckCapacity.crx2\
@@ -111,7 +124,7 @@ class Query:
 						fuelDeposit.date_added \
 						FROM fuelDeposit JOIN truckCapacity \
 						ON (truckCapacity.id_citern = fuelDeposit.idCitern)\
-						WHERE station_id = \''+str(self.stationId)+'\' \
+						WHERE station_id = \''+str(station_id)+'\' \
 						AND date_added BETWEEN(CURDATE() - INTERVAL '+period[periods]+') AND CURDATE()'}
 
 
@@ -121,9 +134,10 @@ class Query:
 							"Fuel Deposit": ("ID Citern","Expected (Gal)","Date")}
 
 		command = tables[table]
-		cursor = self.db.cursor()
-		cursor.execute(command)
-		return cursor.fetchall()
+		self.cursor.execute(command)
+		data = self.cursor.fetchall()
+		self.db.close()
+		return data
 
 
 	def refresh_db(self):
